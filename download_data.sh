45d6e7996a1ba12995c48b13feb581dcc5631c86
Rémi Dupré <remim.dupre@gmail.com>
2019-11-09 18:29:12 +0100

    travis: create data folder

--- download_data.sh
+++ download_data.sh
@@ -8,14 +8,15 @@ git clone --branch usability \
     && npm run crawlit \
     && node ./dist/app.js -g dofus -l english -c allequipments --all \
     && node ./dist/app.js -g dofus -l english -c allweapons --all \
-    && node ./dist/app.js -g dofus -l english -c set --all \
+    && node ./dist/app.js -g dofus -l english -c mount --all \
     && node ./dist/app.js -g dofus -l english -c pet --all \
-    && node ./dist/app.js -g dofus -l english -c mount --all )
+    && node ./dist/app.js -g dofus -l english -c set --all )
 
+mkdir -p data
 cp crawlit/data/dofus/allequipments.json data/equipments.json
 cp crawlit/data/dofus/allweapons.json    data/weapons.json
-cp crawlit/data/dofus/pet.json           data/pets.json
 cp crawlit/data/dofus/mount.json         data/mounts.json
+cp crawlit/data/dofus/pet.json           data/pets.json
 cp crawlit/data/dofus/set.json           data/sets.json
 
 rm -rf crawlit
